20d28c5a8d81932b00f047dd058a1cccde175417
Adam123456999 <adammambetov.342@gmail.com>
2022-05-24 20:24:55 +0300

    Use CORS

--- src/main/kotlin/com/example/plugins/HTTP.kt
+++ src/main/kotlin/com/example/plugins/HTTP.kt
@@ -1,17 +1,19 @@
 package com.example.plugins
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureHTTP() {
+  val port = HoconApplicationConfig(ConfigFactory.load()).property("ktor.deployment.port")
   install(CORS) {
     allowMethod(HttpMethod.Delete)
     allowHeader(HttpHeaders.AccessControlAllowHeaders)
     allowHeader(HttpHeaders.ContentType)
     allowHeader(HttpHeaders.AccessControlAllowOrigin)
     allowHeader(HttpHeaders.Authorization)
-//    allowHost("examplehost:8080", schemes = listOf("https"))
-    anyHost()
+    allowHost("glacial-lowlands-26951.herokuapp.com:$port", schemes = listOf("https"))
   }
 }
